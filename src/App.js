9dce62923bad699dfa91ac92aaa7ffb1e6f9b1c9
gordun209 <ali-han80@hotmail.com>
2022-02-18 15:44:42 +0300

    materual/ui table formatted for app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getCoins } from "./features/coinsSlice";
 import Spinner from "./components/Spinner";
+import CoinsList from "./features/CoinsList";
+import StickyHeadTable from "./components/Tables";
 function App() {
-  const dispatch = useDispatch();
-
-  const [showMore, setShowMore] = React.useState(false);
   const status = useSelector((state) => state.reducer.status);
   const coins = useSelector((state) => state.reducer.coins);
-  const handleClick = () => {
-    setShowMore(!showMore);
-  };
 
   return (
     <>
-      <button onClick={() => dispatch(getCoins())}>Get Coins</button>
-      <ul>
-        {status === "loading" && <Spinner />}
-        {status === "error" && <li>Error!</li>}
-        {status === "success" &&
-          coins.map((coin) => (
-            <div>
-              <img src={coin.icon} style={{ width: "40px" }} alt="" />
-              <li key={coin.id}>
-                name :{coin.name} price: {coin.price.toFixed(3)}
-              </li>
-              <button onClick={() => handleClick(coin.id)}>
-                {showMore ? "showLess" : "showMore"}
-              </button>
-            </div>
-          ))}
-      </ul>
+      {/*<CoinsList />*/}
+      <StickyHeadTable />
     </>
   );
 }
--- src/features/CoinsList.jsx
+++ src/features/CoinsList.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-
-const CoinsList = ({ coins, onCoinClick }) => {
+import { useDispatch, useSelector } from "react-redux";
+import { getCoins } from "./coinsSlice";
+import { Button } from "@mui/material";
+const CoinsList = () => {
+  const coins = useSelector((state) => state.reducer.coins);
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.reducer.status);
   return (
     <div className="coins-list">
-      {coins.map((coin) => (
-        <div
-          key={coin.id}
-          className="coin"
-          onClick={() => onCoinClick(coin.id)}
-        >
-          <img src={coin.imageUrl} alt={coin.name} />
-          <div className="coin-name">{coin.name}</div>
-        </div>
-      ))}
+      <Button
+        variant="contained"
+        onClick={() => dispatch(getCoins())}
+        color="success"
+      >
+        Get coins
+      </Button>
+      {status === "loading" ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <div key={coin.id}>
+              <img src={coin.icon} style={{ width: "35px" }} alt={coin.name} />
+              <li key={coin.id}>
+                {coin.name} {coin.price}
+              </li>
+            </div>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
